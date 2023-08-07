dafaaf24b836b6304182457724aebef608d5b5c3
Mostafa Eid <114962092+Mostafa-Eid2@users.noreply.github.com>
2023-08-07 20:59:02 +0000

    latest monday , still having pk problems

--- table_scripts/create_table_test.sh
+++ table_scripts/create_table_test.sh
@@ -26,9 +26,6 @@ function create_table {
         # File path for the table
         table_file="databases/$db_name/$table_name.txt"
 
-        # File path for the table
-        table_file="databases/$table_name.txt"
-
         # Check if the table file already exists
         if [ -e "$table_file" ]; then
             echo "Table file '$table_file' already exists."
@@ -91,7 +88,6 @@ function create_table {
         # Check if the directory is readable and writable
         if [ ! -r "databases/$db_name" ] || [ ! -w "databases/$db_name" ]; then
             echo "Warning: The 'databases/$db_name' directory doesn't have read/write permissions."
-
             # Prompt the user if they want to grant the needed permissions
             read -p "Do you want to grant the necessary permissions to the directory? (y/n): " permission_response
             if [[ "$permission_response" != "y" && "$permission_response" != "Y" ]]; then
@@ -107,62 +103,69 @@ function create_table {
         # Create the table file in the databases/$db_name directory
         touch "databases/$db_name/$table_name.txt"
 
-        # Set permissions for the file (table_name.txt)
+        #create the metadata file
+        touch "databases/$db_name/.$(echo "$table_name" | tr -d ' ' )_md.txt" 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Error creating metadata file: $?"
+        fi
+
+        # Set permissions for the file (table_name.txt) and metadata file
         chmod 644 "databases/$db_name/$table_name.txt"
+        chmod 644 "databases/$db_name/.$(echo "$table_name" | tr -d ' ' )_md.txt"
 
         # Inform the user that the table is created successfully
         echo "Table '$table_name' created successfully in the 'databases/$db_name' directory."
 
 
-        # ##############################################################################
+       # Function to prompt the user for column names and data types
+function prompt_column_data {
+    while true; do
+        read -r -p "Please specify the column names and data types for the table (e.g., column_1_name:i, column_2_name:f, column_3_name:s):
+    Valid data type shortcuts: i (int), f (float), s (string)    >>>>    Don't use brackets
+    Enter here: " column_data_input
 
+        # Convert user input to lowercase and remove spaces
+        column_data_input=$(echo "$column_data_input" | tr '[:upper:]' '[:lower:]' | tr -d ' ')
 
-        # Function to prompt the user for column names and data types
-        function prompt_column_data {
-            read -p "Please specify the column names and data types for the table (e.g., column_1_name:i, column_2_name:s, column_3_name:d): 
-            " column_data_input
+        # Split the input into an array
+        IFS=',' read -r -a columns_and_types <<< "$column_data_input"
 
-            # Convert user input to lowercase and remove spaces
-            column_data_input=$(echo "$column_data_input" | tr '[:upper:]' '[:lower:]' | tr -d ' ')
+        # Validate and store column names and data types
+        declare -a column_names
+        declare -a data_types
 
-            # Split the input into an array
-            IFS=',' read -r -a columns_and_types <<< "$column_data_input"
+        # Define an associative array to map data type shortcuts to full data type names
+        declare -A data_type_map
+        data_type_map["i"]="int"
+        data_type_map["f"]="float"
+        data_type_map["s"]="string"
 
-            # Validate and store column names and data types
-            declare -a column_names
-            declare -a data_types
+        for column_and_type in "${columns_and_types[@]}"; do
+            # Extract column name and data type from input
+            column_name=$(echo "$column_and_type" | cut -d ":" -f 1)
+            data_type_shortcut=$(echo "$column_and_type" | cut -d ":" -f 2)
 
-            # Define an associative array to map data type shortcuts to full data type names
-            declare -A data_type_map
-            data_type_map["i"]="int"
-            data_type_map["l"]="long"
-            data_type_map["d"]="double"
-            data_type_map["s"]="string"
-            data_type_map["b"]="boolean"
+            # Map the data type shortcut to its full data type name
+            data_type="${data_type_map[$data_type_shortcut]}"
 
-                        #Loop over each column and type input
-            for column_and_type in "${columns_and_types[@]}"; do
-                while true; do
-                    # Extract column name and data type from input
-                    column_name=$(echo "$column_and_type" | cut -d ":" -f 1)
-                    data_type_shortcut=$(echo "$column_and_type" | cut -d ":" -f 2)
+            # Check if the data type is valid
+            if [ -z "$data_type" ]; then
+                echo "Error: Invalid data type shortcut '$data_type_shortcut'."
+                echo "Valid data type shortcuts: i (int), f (float), s (string)"
+            else
+                # Valid data type, add column name and data type to respective arrays
+                column_names+=("$column_name")
+                data_types+=("$data_type")
+            fi
+        done
 
-                    # Map the data type shortcut to its full data type name
-                    data_type="${data_type_map[$data_type_shortcut]}"
-
-                    # Check if the data type is valid
-                    if [ -z "$data_type" ]; then
-                        echo "Error: Invalid data type shortcut '$data_type_shortcut'."
-                        echo "Valid data type shortcuts: i (int), l (long), d (double), s (string), b (boolean)"
-                        read -p "Please enter a valid data type shortcut: " data_type_shortcut
-                    else
-                        # Valid data type, add column name and data type to respective arrays
-                        column_names+=("$column_name")
-                        data_types+=("$data_type")
-                        break
-                    fi
-                done
-            done
+        # Check if all inputs were valid, if so, exit the loop
+        if [ "${#columns_and_types[@]}" -eq "${#column_names[@]}" ]; then
+            break
+        else
+            echo "Invalid input. Please provide the correct format for each column (e.g., column_name:data_type)."
+        fi
+    done
 
             # Display the column names and their mapped data types to the user
             echo "You've entered the following columns and their data types:"
@@ -171,52 +174,72 @@ function create_table {
             done
         }
 
-
-        # ##############################################################################
-
 # Call the prompt_column_data function
 prompt_column_data
 
-        # Prompt for the primary key column
-        echo "Please choose the primary key column from the list below:"
+#****************************************************************************************
+#****************************************************************************************
+#****************************************************************************************
+#****************************************************************************************
+#****************************************************************************************
+
+       # Prompt for the primary key column
+echo "Please choose the primary key column from the list below:"
+for i in "${!column_names[@]}"; do
+    echo "$((i+1)): ${column_names[i]}"
+done
+
+while true; do
+    read -r -p "Enter the number or name of the primary key column (Type 'exit' to delete the table and return to the main menu): " primary_key_input
+
+    # Check if the user wants to exit and delete the table
+    if [ "$primary_key_input" = "exit" ]; then
+        echo "Deleting the table and returning to the main menu..."
+        # Remove the table file if it exists
+        rm -f "$table_file"
+        exit 0
+    fi
+
+    # Check if the input is a valid column number
+    if [[ $primary_key_input =~ ^[0-9]+$ ]]; then
+        if [ "$primary_key_input" -ge 1 ] && [ "$primary_key_input" -le ${#column_names[@]} ]; then
+            # Subtract 1 from the input to get the index in the arrays
+            primary_key_index=$((primary_key_input-1))
+            break
+        fi
+    fi
+
+    # Check if the input is a valid column name
+    if [[ "${column_names[@]}" =~ "$primary_key_input" ]]; then
+        primary_key_index=-1
         for i in "${!column_names[@]}"; do
-            echo "$((i+1)): ${column_names[i]}"
-        done
-
-        while true; do
-            read -r -p "Enter the number or name of the primary key column (Type 'exit' to delete the table and return to the main menu): " primary_key_input
-
-            # Check if the user wants to exit and delete the table
-            if [ "$primary_key_input" = "exit" ]; then
-                echo "Deleting the table and returning to the main menu..."
-                # Remove the table file if it exists
-                rm -f "$table_file"
-                exit 0
-            fi
-
-            # Check if the input is a valid column number
-            if [[ $primary_key_input =~ ^[0-9]+$ && $primary_key_input -ge 1 && $primary_key_input -le ${#column_names[@]} ]]; then
-                # Subtract 1 from the input to get the index in the arrays
-                primary_key_index=$((primary_key_input-1))
+            if [ "${column_names[i]}" = "$primary_key_input" ]; then
+                primary_key_index=$i
                 break
-            elif [[ " ${column_names[@]} " =~ " $primary_key_input " ]]; then
-                # Check if the input is a valid column name
-                primary_key_index=$(echo "${column_names[@]}" | tr ' ' '\n' | grep -n "^$primary_key_input$" | cut -d ":" -f 1)
-                break
-            else
-                echo "Error: Invalid input. Please choose a valid column number or name."
             fi
         done
+        if [ "$primary_key_index" -ge 0 ]; then
+            break
+        fi
+    fi
 
-        # Get the selected primary key column and data type
-        primary_key_column=${column_names[primary_key_index]}
-        primary_key_data_type=${data_types[primary_key_index]}
+    echo "Error: Invalid input. Please choose a valid column number or name."
+done
+
+echo "Success: Primary key selected for column ${column_names[primary_key_index]}"
+
+# Get the selected primary key column and data type
+primary_key_column=${column_names[primary_key_index]}
+primary_key_data_type=${data_types[primary_key_index]}
+
+
 
         break
     done
 
     # Your primary key implementation goes here...
 
+        # ##############################################################################
 }
 
 # Call the function to create a new table
